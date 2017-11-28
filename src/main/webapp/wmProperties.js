c4155ac6e2e589b120f5c9ff7239b4408c7847ae
ZMlWxmArSN <automate1@wavemaker.com>
2017-11-28 22:35:40 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_MUNZxOvIRz",
+  "homePage" : "Main",
+  "name" : "Auto_MUNZxOvIRz",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
